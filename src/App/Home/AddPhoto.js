1edae10590c198280012951f36e670a3ab10857f
Manuel Casares <manucasaresrojas@gmail.com>
2021-08-23 15:04:52 -0300

    Eliminado valores por defecto en addphoto y agregado .env.production

--- src/App/Home/AddPhoto.js
+++ src/App/Home/AddPhoto.js
@@ -18,9 +18,8 @@ import toast from 'react-hot-toast';
 
 export const AddPhoto = () => {
 	const [{ label, photo_url }, handleInputChange] = useForm({
-		label: 'Paisaje',
-		photo_url:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png',
+		label: '',
+		photo_url: '',
 	});
 
 	const dispatch = useDispatch();
